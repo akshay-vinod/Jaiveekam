89ae254dbb8401f8d732efc0dd1a0c9456785318
akshay-vinod <akshayvinod468@gmail.com>
2021-02-07 16:04:08 +0530

    nav bar updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,33 @@
 import React from "react";
-
+import "./Nav.css";
 const Nav = () => {
-  return <p>hai</p>;
+  return (
+    <div className="alignitems">
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About Us </a>
+            </li>
+            <li>
+              <a href="#">Buy/Sell</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">People</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 };
 export default Nav;
