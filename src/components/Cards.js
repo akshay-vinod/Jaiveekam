3ac7bf5279bde2803237874fe662aa5fddd51b1a
navaneethmanoj <navaneethpm@gmail.com>
2021-02-09 16:36:08 +0530

    Modified cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Cards.css";
 const Cards = () => {
   return (
-    <div className="container farmer">
+    <div className="container">
       <div className="row">
         <h4 className="display-5">Creating opportunities for everyone</h4>
       </div>
@@ -12,29 +12,47 @@ const Cards = () => {
           retailers, and consumers.
         </p>
       </div>
-      <div className="row">
+      <div className="row mt-5">
         <div className="col-3">
           <div className="card cards">
-            <img className="farmer" src="/images/farmer.svg" alt="" />
-            <h5 className="mt-2 font-weight-bold">Benefits for farmers</h5>
+            <div className="card-body">
+              <img className="icons" src="/images/farmer.svg" alt="" />
+              <h4 className="card-title">Benefits for farmers</h4>
+              <div className="card-text">
+              <p>Competetive Pricing</p>
+              <p>Doorstep delivery</p>
+              <p>High quality graded produce</p>
+              <p>Transparent weighing</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="col-3">
           <div className="card cards">
-            <h5 className="mt-2">Convenience for retailers</h5>
-            <p>Competetive Pricing</p>
-            <p>Doorstep delivery</p>
-            <p>High quality graded produce</p>
-            <p>Transparent weighing</p>
+            <div className="card-body">
+            <img className="icons" src="/images/undraw_business_shop_qw5t.svg" alt="" />
+              <h4 className="card-title">Convenience for retailers</h4>
+              <div className="card-text">
+              <p>Competetive Pricing</p>
+              <p>Doorstep delivery</p>
+              <p>High quality graded produce</p>
+              <p>Transparent weighing</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="col-3">
           <div className="card cards">
-            <h5 className="mt-2">Savings for consumers</h5>
-            <p>Competetive Pricing</p>
-            <p>Doorstep delivery</p>
-            <p>High quality graded produce</p>
-            <p>Transparent weighing</p>
+            <div className="card-body">
+            <img className="icons" src="/images/undraw_successful_purchase_uyin.svg" alt="" />
+            <h4 className="card-title">Savings for consumers</h4>
+            <div className="card-text">
+              <p>Competetive Pricing</p>
+              <p>Doorstep delivery</p>
+              <p>High quality graded produce</p>
+              <p>Transparent weighing</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
