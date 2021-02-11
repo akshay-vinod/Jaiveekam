524542f44297a93ac33e7fbef6139bf2ee8a16a3
navaneethmanoj <navaneethpm@gmail.com>
2021-02-11 17:51:51 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,25 +2,75 @@ import React from "react";
 import "./Footer.css";
 const Footer = () => {
   return (
-    <div className="footerStyle">
+    <div>
       <div>
         <img className="footerBg" src="/images/footerbg.svg" alt="" />
       </div>
       <div className="container">
-        <div className="row">
+        <div className="row footer">
           <div className="col-4">
-            <h5 className="text-dark">Contact us</h5>
+            <h4>Contact us</h4>
             <hr />
-            <p>Phone number:</p>
-            <p>Email id</p>
+            <p>
+              <i class="fa fa-phone fa-2x  fa-margin" aria-hidden="true"></i>
+              +91 9232323232
+            </p>
+            <p>
+              <i class="fa fa-envelope fa-2x fa-margin" aria-hidden="true"></i>
+              johndoe@gmail.com
+            </p>
             <p className="text-wrap">
-              Address in lines ahshashashsahshasahhsahshashas
-              hashashsaswertyucucuaucuaucauusascmcmcmcmmc
+              <i
+                className="fa fa-map-marker fa-2x fa-margin"
+                aria-hidden="true"
+              ></i>{" "}
+              90B, Delhi - Jaipur Expy, Udyog Vihar{" "}
+              <span style={{ "padding-left": "50px" }}>
+                {" "}
+                Sector 18, Gurugram, Haryana 122008
+              </span>
             </p>
           </div>
-          <div className="col-4">
-            <h5>Connect with us</h5>
+          <div className="col-3 connect">
+            <h4>Connect with us</h4>
             <hr />
+            <a
+              href="#"
+              className="fa fa-facebook-official fa-fw fa-2x fa-margin ml-5"
+              aria-hidden="true"
+            ></a>
+            <a
+              href="#"
+              className="fa fa-linkedin-square fa-fw fa-2x fa-margin"
+              aria-hidden="true"
+            ></a>
+            <a href="#" className="fab fa-instagram fa-fw fa-2x fa-margin"></a>
+            <a
+              href="#"
+              className="fab fa-twitter-square fa-fw fa-2x fa-margin"
+            ></a>
+          </div>
+          <div className="col-1"></div>
+          <div className="col-4">
+            <h4>Quick Links</h4>
+            <hr />
+            <div className="links">
+              <p>
+                <a href="#">About Us</a>
+              </p>
+              <p>
+                <a href="#">Buy/Sell</a>
+              </p>
+              <p>
+                <a href="#">People</a>
+              </p>
+              <p>
+                <a href="#">Blogs</a>
+              </p>
+              <p>
+                <a href="#">FAQ</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Front.js
+++ src/components/Front.js
@@ -9,12 +9,10 @@ const Front = () => {
           <div className="col-4 mt-5 ml-3">
             <h2>Revolutionizing the Fresh Produce Supply Chain</h2>
             <br />
-            <display-3>
+            <p>
               Driven by cutting edge technology, we source fresh produce from
               farmers and sell to businesses, across India
-            </display-3>
-            <br />
-            <br />
+            </p>
             <button className="btn btn-success">Learn more</button>
           </div>
         </div>
